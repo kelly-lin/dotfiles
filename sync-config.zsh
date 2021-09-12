946bcc287cda30cb80d632129a7bc848a0e2d397
Kelly Lin <findkellylin@gmail.com>
2021-09-12 13:18:25 +1000

    update files

---
+++ sync-config.zsh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+echo "Please enter the number for the config to sync (1 - zsh, 2 - tmux, 3 - vim, 4 - all)"
+read config
+case $config in
+  1)
+    echo "Updating zsh config..."
+    files=(".zshrc")
+    ;;
+  2)
+    echo "Updating tmux config..."
+    files=(".tmux.conf")
+    ;;
+  3)
+    echo "Updating vim config..."
+    files=(".vimrc")
+    ;;
+  4)
+    echo "Updating all configs"
+    files=(".zshrc" ".tmux.conf" ".vimrc")
+    ;;
+esac
+
+echo "Pulling remote repository"
+git reset --hard HEAD
+git checkout master
+git pull origin master
+
+echo "Copying files..."
+for filename in "${files[@]}"
+do
+  cp ./$filename ~/$filename 
+done
+
+echo "Sync complete"
