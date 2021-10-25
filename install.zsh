b744c430cf02ec74b5ebc4ba95d26dbd5fe664ed
Kelly Lin <findkellylin@gmail.com>
2021-10-25 19:27:28 +1100

    modify install script

--- install.zsh
+++ install.zsh
@@ -1,42 +1,16 @@
 #!/bin/zsh
-install_vim="./scripts/install-vim-config.zsh"
-install_tmux="./scripts/install-tmux-config.zsh"
-install_zsh="./scripts/install-zsh-config.zsh"
 
-echo "Please enter the number for the config to sync (1 - zsh, 2 - tmux, 3 - vim, 4 - all)"
-read config
-case $config in
-  1)
-    echo "Updating zsh config..."
-    installs=($install_zsh)
-    ;;
-  2)
-    echo "Updating tmux config..."
-    installs=($install_tmux)
-    ;;
-  3)
-    echo "Updating vim config..."
-    installs=($install_vim)
-    ;;
-  4)
-    echo "Updating all configs"
-    installs=($install_vim $install_zsh $install_tmux)
-    ;;
-  *)
-    echo "Invalid selection, exiting..."
-    exit 1
-esac
+echo "Installing alacritty config"
+ln -s ./.alacritty.yml ~/.alacritty.yml
 
-echo "Pulling remote repository"
-git reset --hard HEAD
-git checkout master
-git pull origin master
+echo "Installing tmux config"
+ln -s ./.tmux.conf ~/.tmux.conf
 
-echo "Copying files..."
-for install in "$installs[@]"
-do
-  source install
-done
+echo "Installing zsh config"
+ln -s ./.zshrc ~/.zshrc
+
+mkdir -p ~/.zsh
+ln -s ./.alias.zsh ~/.zsh/.alias.zsh
 
 echo "Install complete"
 exit 0
