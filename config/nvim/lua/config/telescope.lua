9c2c4dead1e6082a9d1951648b757155dec18f5f
Kelly Lin <kelly.lin@nine.com.au>
2022-01-24 09:25:34 +1100

    add telescope-fzf-native

--- config/nvim/lua/config/telescope.lua
+++ config/nvim/lua/config/telescope.lua
@@ -1,5 +1,6 @@
-local actions = require("telescope.actions")
-require('telescope').setup{
+local actions = require('telescope.actions')
+
+require('telescope').setup {
   defaults = {
     mappings = {
       i = {
@@ -11,4 +12,15 @@ require('telescope').setup{
       }
     }
   },
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
+  }
 }
+
+require('telescope').load_extension('fzf')
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -18,6 +18,7 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/popup.nvim'}, { 'nvim-lua/plenary.nvim' } }
   }
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
   use { 'neovim/nvim-lspconfig' }
   use { 'nvim-lua/completion-nvim' }
 
