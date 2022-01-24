d4fe4c496aaf3c9d77b18c8cd26063ca1168fe83
Kelly Lin <findkellylin@gmail.com>
2022-01-24 19:24:34 +1100

    remove luasnip

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -37,9 +37,6 @@ require('packer').startup(function(use)
   use { 'hrsh7th/cmp-cmdline' }
   use { 'hrsh7th/nvim-cmp' }
 
-  -- use { 'L3MON4D3/LuaSnip' }
-  -- use { 'saadparwaiz1/cmp_luasnip' }
-
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   use { 'airblade/vim-gitgutter' }
   use { 'christoomey/vim-tmux-navigator' }
