1cd538271eab26107ea4e876bfdce14977ff8b38
Kelly Lin <findkellylin@gmail.com>
2022-01-24 22:30:10 +1100

    change lualine to show full path of current file

--- config/nvim/lua/config/lualine.lua
+++ config/nvim/lua/config/lualine.lua
@@ -2,5 +2,14 @@ require('lualine').setup {
   options = {
     theme = 'onedark',
     disabled_filetypes = { 'NvimTree', 'Outline' }
+  },
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        file_status = true,
+        path = 2
+      }
+    },
   }
 }
