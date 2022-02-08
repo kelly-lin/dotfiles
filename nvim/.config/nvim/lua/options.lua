08348352065f417e79a7f4d5679e0598bf5d8e71
Kelly Lin <kelly.lin@nine.com.au>
2022-02-09 09:39:44 +1100

    split keymaps and options to separate lua files

---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,58 @@
+local cmd = vim.cmd -- to execute Vim commands e.g. cmd('pwd')
+local fn = vim.fn -- a table to access global variables
+local g = vim.g -- a table to access global variables
+local opt = vim.opt -- to set options-- Map leader to space
+
+opt.cursorline = true
+opt.signcolumn = "yes"
+opt.encoding = "utf-8"
+opt.showmode = false
+
+opt.wildignore:append({ "**/node_modules/*", "**/android/*", "**/ios/*", "**/.git/*" })
+
+opt.expandtab = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+
+opt.smartindent = true
+opt.autoindent = true
+opt.incsearch = true
+opt.showcmd = true
+opt.number = true
+opt.hlsearch = false
+opt.hidden = true
+opt.ruler = true
+opt.number = true
+opt.relativenumber = true
+opt.backspace = { "indent", "eol", "start" }
+opt.wrap = false
+
+opt.backup = false
+opt.writebackup = false
+opt.swapfile = false
+opt.undofile = true
+
+local undodir = vim.fn.stdpath("data") .. "/.undo"
+opt.undodir = undodir
+
+opt.textwidth = 80
+opt.colorcolumn = "80"
+
+-- Autocommands
+cmd("autocmd FileType gitcommit set colorcolumn+=51")
+cmd("autocmd FileType gitcommit set colorcolumn=73")
+
+-- Keybindings
+-- Leader key
+g.mapleader = " "
+
+-- nvim-cmp
+opt.completeopt = { "menu", "menuone", "noselect" }
+
+if fn.has("macunix") then
+	opt.clipboard = "unnamed"
+end
+if fn.has("unix") then
+	opt.clipboard = "unnamedplus"
+end
