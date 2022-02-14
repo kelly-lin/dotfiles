ce08583f5d5496dd22a1b41a0ed0984a8ba43bec
Kelly Lin <kelly.lin@nine.com.au>
2022-02-14 14:23:54 +1100

    change error in diff alias

--- zsh/.config/zsh/alias.zsh
+++ zsh/.config/zsh/alias.zsh
@@ -10,7 +10,6 @@ alias la='ls -ah'
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
-alias diff='diff --color=auto'
 alias ip='ip --color=auto'
 
 alias vim='nvim'
