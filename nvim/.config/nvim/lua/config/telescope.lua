29712782759df053ae3a7e0782236041ab7d68f8
Kelly Lin <kelly.lin@nine.com.au>
2022-02-15 14:21:49 +1100

    remove redundant code

--- nvim/.config/nvim/lua/config/telescope.lua
+++ nvim/.config/nvim/lua/config/telescope.lua
@@ -9,9 +9,6 @@ telescope.setup({
 	defaults = {
 		mappings = {
 			i = {
-				-- map actions.which_key to <C-h> (default: <C-/>)
-				-- actions.which_key shows the mappings for your picker,
-				-- e.g. git_{create, delete, ...}_branch for the git_branches picker
 				["<C-k>"] = actions.move_selection_previous,
 				["<C-j>"] = actions.move_selection_next,
 			},
@@ -23,7 +20,6 @@ telescope.setup({
 			override_generic_sorter = true, -- override the generic sorter
 			override_file_sorter = true, -- override the file sorter
 			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-			-- the default case_mode is "smart_case"
 		},
 	},
 })
