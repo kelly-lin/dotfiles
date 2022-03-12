fee5d16e045bc373a157b2abc07ed5a0d9142de9
Kelly Lin <kelly.lin@nine.com.au>
2022-03-12 11:17:42 +1100

    add ghq alias

--- zsh/.config/zsh/alias.zsh
+++ zsh/.config/zsh/alias.zsh
@@ -29,6 +29,7 @@ alias evim='$EDITOR ~/.config/nvim/init.vim'
 alias ezsh='$EDITOR ~/.zshrc'
 alias szsh='source ~/.zshrc'
 
+alias grp="cd ~/ghq"
 alias rp="cd ~/Repos"
 alias prp="cd ~/Repos/personal"
 
