e38b27b544d7bba6b206f6582f7cb245b5e1a44e
Kelly Lin <kelly.lin@nine.com.au>
2022-03-17 18:13:39 +1100

    move luasnip to cmp

--- nvim/.config/nvim/lua/config/completion.lua
+++ nvim/.config/nvim/lua/config/completion.lua
@@ -3,20 +3,64 @@ if not cmp_loaded then
 	return
 end
 
+local luasnip_loaded, luasnip = pcall(require, "luasnip")
+if not luasnip_loaded then
+	return
+end
+
+local t = function(str)
+	return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+	local col = vim.fn.col(".") - 1
+	if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+		return true
+	else
+		return false
+	end
+end
+
+_G.tab_complete = function()
+	if cmp and cmp.visible() then
+		cmp.select_next_item()
+	elseif luasnip and luasnip.expand_or_jumpable() then
+		return t("<Plug>luasnip-expand-or-jump")
+	elseif check_back_space() then
+		return t("<Tab>")
+	else
+		cmp.complete()
+	end
+	return ""
+end
+
+_G.s_tab_complete = function()
+	if cmp and cmp.visible() then
+		cmp.select_prev_item()
+	elseif luasnip and luasnip.jumpable(-1) then
+		return t("<Plug>luasnip-jump-prev")
+	else
+		return t("<S-Tab>")
+	end
+	return ""
+end
+
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 cmp.setup({
 	snippet = {
 		expand = function(args)
-			-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
 			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 		end,
 	},
 	mapping = {
 		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
 		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
 		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-		["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+		["<C-y>"] = cmp.config.disable,
 		["<C-e>"] = cmp.mapping({
 			i = cmp.mapping.abort(),
 			c = cmp.mapping.close(),
@@ -25,6 +69,10 @@ cmp.setup({
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
 			else
 				fallback()
 			end
@@ -32,6 +80,8 @@ cmp.setup({
 		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
 			else
 				fallback()
 			end
@@ -39,10 +89,7 @@ cmp.setup({
 	},
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
-		{ name = "luasnip" }, -- For luasnip users.
-		-- { name = 'vsnip' }, -- For vsnip users.
-		-- { name = 'ultisnips' }, -- For ultisnips users.
-		-- { name = 'snippy' }, -- For snippy users.
+		{ name = "luasnip" },
 	}, {
 		{ name = "buffer" },
 	}),
