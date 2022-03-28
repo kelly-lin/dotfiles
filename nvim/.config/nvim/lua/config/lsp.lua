1214dc7a803e8f2cc08908b3b07ed1bc5cd358c6
Kelly Lin <kelly.lin@nine.com.au>
2022-03-29 09:55:29 +1100

    remove code for disabling inline diagnostics

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -77,10 +77,3 @@ for _, lsp in ipairs(servers) do
 		settings = settings,
 	})
 end
-
--- Disable nvim lsp inline diagnostics
--- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
---   vim.lsp.diagnostic.on_publish_diagnostics, {
---     virtual_text = false
---   }
--- )
