83dece093efda06a26c774a5ff7bf2dd7c082f87
Kelly Lin <kelly.lin@nine.com.au>
2022-04-04 09:54:36 +1000

    remove toggleterm

--- nvim/.config/nvim/plugin/packer_compiled.lua
+++ nvim/.config/nvim/plugin/packer_compiled.lua
@@ -240,11 +240,6 @@ _G.packer_plugins = {
     path = "/Users/kelly.lin/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
-  ["toggleterm.nvim"] = {
-    loaded = true,
-    path = "/Users/kelly.lin/.local/share/nvim/site/pack/packer/start/toggleterm.nvim",
-    url = "https://github.com/akinsho/toggleterm.nvim"
-  },
   undotree = {
     loaded = true,
     path = "/Users/kelly.lin/.local/share/nvim/site/pack/packer/start/undotree",
