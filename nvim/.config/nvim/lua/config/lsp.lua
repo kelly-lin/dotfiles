dd3d84a44ad7c72d97f9f19db9b3b14fc34f3851
Kelly Lin <kelly.lin@nine.com.au>
2022-04-04 17:08:08 +1000

    refactor lsp config

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -5,33 +5,13 @@ end
 
 local bmap = require("utils.keymaps").bmap
 
-local function on_attach(client, bufnr)
-	-- Mappings.
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	bmap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { silent = true })
-	bmap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { silent = true })
-	bmap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { silent = true })
-	bmap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { silent = true })
-	bmap(bufnr, "n", "<leader>df", "<cmd>lua vim.lsp.buf.formatting()<CR>", { silent = true })
-	-- bmap(bufnr, 'n', '<C-i>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', { silent = true })
-	bmap(bufnr, "n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", { silent = true })
-	bmap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { silent = true })
-	bmap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { silent = true })
-	bmap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", { silent = true })
-
-	client.resolved_capabilities.document_formatting = false
-	client.resolved_capabilities.document_range_formatting = false
-end
-
 local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
-local servers = { "sumneko_lua", "pyright", "tsserver", "jsonls", "vimls", "yamlls", "solargraph" }
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
-for _, lsp in ipairs(servers) do
-	local settings = {}
-	if lsp == "sumneko_lua" then
+local servers = {
+	{
+		"sumneko_lua",
 		settings = {
 			Lua = {
 				runtime = {
@@ -53,10 +33,14 @@ for _, lsp in ipairs(servers) do
 					enable = false,
 				},
 			},
-		}
-	end
-
-	if lsp == "yamlls" then
+		},
+	},
+	"pyright",
+	"tsserver",
+	"jsonls",
+	"vimls",
+	{
+		"yamlls",
 		settings = {
 			yaml = {
 				schemas = {
@@ -65,11 +49,44 @@ for _, lsp in ipairs(servers) do
 					["/path/from/root/of/project"] = "/.github/workflows/*",
 				},
 			},
-		}
-	end
+		},
+	},
+	"solargraph",
+}
 
-	nvim_lsp[lsp].setup({
-		on_attach = on_attach,
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+local function getServerConfig(server)
+	local server_name = ""
+	local settings = {}
+	if type(server) == "table" then
+		server_name = server[1]
+		settings = server.settings
+	elseif type(server) == "string" then
+		server_name = server
+	else
+		error("the specified server must be either a string or table")
+	end
+	return server_name, settings
+end
+
+for _, server in ipairs(servers) do
+	local server_name, settings = getServerConfig(server)
+	nvim_lsp[server_name].setup({
+		on_attach = function(client, bufnr)
+			bmap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { silent = true })
+			bmap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { silent = true })
+			bmap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { silent = true })
+			bmap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { silent = true })
+			bmap(bufnr, "n", "<leader>df", "<cmd>lua vim.lsp.buf.formatting()<CR>", { silent = true })
+			bmap(bufnr, "n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", { silent = true })
+			bmap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { silent = true })
+			bmap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { silent = true })
+			bmap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", { silent = true })
+
+			client.resolved_capabilities.document_formatting = false
+			client.resolved_capabilities.document_range_formatting = false
+		end,
 		capabilities = capabilities,
 		flags = {
 			debounce_text_changes = 150,
