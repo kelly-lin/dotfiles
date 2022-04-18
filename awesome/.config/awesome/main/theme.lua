d4ddb78abef6d0d368305c07f9796d3bd0bee30a
Kelly Lin <findkellylin@gmail.com>
2022-04-18 12:38:18 +1000

    remove useless comment

--- awesome/.config/awesome/main/theme.lua
+++ awesome/.config/awesome/main/theme.lua
@@ -1,6 +1,5 @@
 local beautiful = require("beautiful")
 local gears = require("gears")
--- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
+
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 beautiful.useless_gap = 10
