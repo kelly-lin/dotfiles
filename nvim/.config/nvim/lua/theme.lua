dfc6c5632cff514a33900c7db306b5c9630ab3d8
Kelly Lin <kelly.lin@nine.com.au>
2022-06-10 10:47:53 +1000

    clean up theme config

--- nvim/.config/nvim/lua/theme.lua
+++ nvim/.config/nvim/lua/theme.lua
@@ -5,20 +5,15 @@ end
 
 onedark.setup({
 	-- Main options --
-	style = "darker", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-	transparent = false, -- Show/hide background
-	term_colors = true, -- Change terminal color as per the selected theme style
-	ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-
-	-- Change code style ---
-	-- Options are italic, bold, underline, none
-	-- You can configure multiple style with comma seperated, For e.g., keywords = 'italic,bold'
-
-	-- Plugins Config --
+	style = "darker",
+	transparent = false,
+	term_colors = true,
+	ending_tildes = false,
+	toggle_style_list = { "darker" },
 	diagnostics = {
-		darker = true, -- darker colors for diagnostic
-		undercurl = false, -- use undercurl instead of underline for diagnostics
-		background = true, -- use background color for virtual text
+		darker = true,
+		undercurl = true,
+		background = true,
 	},
 })
 require("onedark").load()
