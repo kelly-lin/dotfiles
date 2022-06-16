0948017d8220c2bea59c2edf1d074f3aaf38440b
Kelly Lin <kelly.lin@nine.com.au>
2022-06-16 10:10:46 +1000

    move worktree add out of alias function and into shell script

---
+++ zsh/zsh/scripts/worktree-add.zsh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+dir_name=$1
+if [[ -z $dir_name ]]; then
+  echo 'please provide a directory name for the worktree'
+  exit 1
+fi
+
+branch_name=$2
+if [[ -z $branch_name ]]; then
+  echo 'please provide a branch name for the worktree'
+  exit 1
+fi
+
+root_dir=$(git rev-parse --git-dir | sed 's/\.bare.*//')
+cd $root_dir
+
+git show-branch $branch_name &> /dev/null
+[[ $? -ne 0 ]] && git branch $branch_name
+
+git worktree add $dir_name $branch_name
