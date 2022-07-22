1b11956b0b6bee6b746e566f9494ef6c693bb5c2
Kelly Lin <findkellylin@gmail.com>
2022-07-22 15:19:55 +1000

    add telescope themes

--- nvim/.config/nvim/lua/theme.lua
+++ nvim/.config/nvim/lua/theme.lua
@@ -20,9 +20,10 @@ require("onedark").load()
 
 local function setup_custom_highlights()
   -- Set Telescope window colors
-  vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = "#095fa5" })
-  vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = "#095fa5" })
-  vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = "#095fa5" })
+  vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = "#0c7fdd" })
+  vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = "#0c7fdd" })
+  vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = "#0c7fdd" })
+  vim.api.nvim_set_hl(0, "TelescopeTitle", { fg = "#abb2bf" })
 end
 
 setup_custom_highlights()
