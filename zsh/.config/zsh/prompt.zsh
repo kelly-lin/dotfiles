67d46e88cd0e421711a7db57deba70ba5276df9e
Kelly Lin <findkellylin@gmail.com>
2022-07-22 13:54:49 +1000

    add execution time to prompt

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -5,12 +5,26 @@ prompt_ins='%F{39}%~%f%F{243}$(parse_git_branch)%f%F{82} >%f '
 prompt=$prompt_ins
 ZSH_DISABLE_COMPFIX="true" # This is to ignore insecure directories
 
-# Add a new line after every command except the one after the terminal
-  precmd() {
-    precmd() {
+preexec() {
+  timer=$(date +%s%3N)
+}
+
+precmd() {
+  if [ $timer ]; then
+    now=$(date +%s%3N)
+    elapsed=$(($now-$timer))
+
+    export RPROMPT="%F{243}${elapsed}ms %{$reset_color%}"
+    unset timer
+  fi
+
+  # Add a new line after every command except the one after the terminal
+  if [ -z "$NEW_LINE_BEFORE_PROMPT" ]; then
+      NEW_LINE_BEFORE_PROMPT=1
+  elif [ "$NEW_LINE_BEFORE_PROMPT" -eq 1 ]; then
       echo
-    }
-  }
+  fi
+}
 
 # Change zle cursor style when in the different vim modes
 set_prompt() {
