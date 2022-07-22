24f280b9df3c1330dd58ad9739eedad46b12c86b
Kelly Lin <findkellylin@gmail.com>
2022-07-22 15:14:39 +1000

    add telescope boarder colors

--- nvim/.config/nvim/lua/theme.lua
+++ nvim/.config/nvim/lua/theme.lua
@@ -1,19 +1,28 @@
 local onedark_loaded, onedark = pcall(require, "onedark")
 if not onedark_loaded then
-	return
+  return
 end
 
 onedark.setup({
-	-- Main options --
-	style = "darker",
-	transparent = false,
-	term_colors = true,
-	ending_tildes = false,
-	toggle_style_list = { "darker" },
-	diagnostics = {
-		darker = true,
-		undercurl = true,
-		background = true,
-	},
+  -- Main options --
+  style = "darker",
+  transparent = false,
+  term_colors = true,
+  ending_tildes = false,
+  toggle_style_list = { "darker" },
+  diagnostics = {
+    darker = true,
+    undercurl = true,
+    background = true,
+  },
 })
 require("onedark").load()
+
+local function setup_custom_highlights()
+  -- Set Telescope window colors
+  vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = "#095fa5" })
+  vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = "#095fa5" })
+  vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = "#095fa5" })
+end
+
+setup_custom_highlights()
