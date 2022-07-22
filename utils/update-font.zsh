96556501db9dd3427bbbf986b4c54ce97cc244a7
Kelly Lin <findkellylin@gmail.com>
2022-07-22 23:05:51 +1000

    add font updater, update fonts

---
+++ utils/update-font.zsh
@@ -0,0 +1,5 @@
+#/bin/zsh
+
+curl -flo "JetBrains Mono NL Regular Nerd Font Complete Mono.ttf" "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/JetBrainsMono/NoLigatures/Regular/complete/JetBrains%20Mono%20NL%20Regular%20Nerd%20Font%20Complete%20Mono.ttf"
+curl -flo "JetBrains Mono NL Bold Nerd Font Complete Mono.ttf" "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/JetBrainsMono/NoLigatures/Bold/complete/JetBrains%20Mono%20NL%20Bold%20Nerd%20Font%20Complete%20Mono.ttf"
+curl -flo "JetBrains Mono NL Italic Nerd Font Complete Mono.ttf" "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/JetBrainsMono/NoLigatures/Italic/complete/JetBrains%20Mono%20NL%20Italic%20Nerd%20Font%20Complete%20Mono.ttf"
